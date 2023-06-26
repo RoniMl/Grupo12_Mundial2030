cb594bd897c23a17c208860ff21d81ccb31b2123
RoniMl <ronimalamud@gmail.com>
2023-06-25 22:47:29 -0300

    fin sprint 3

--- src/controllers/indexControllers.js
+++ src/controllers/indexControllers.js
@@ -3,5 +3,11 @@ const controller = {
   index: (req, res) => {
     res.render(path.join(__dirname + "../../views/index.ejs")); 
   },
+  fanaticos: (req, res) => {
+    res.render(path.join(__dirname + "../../views/fanaticos.ejs")); 
+  },
+  tickets: (req, res) => {
+    res.render(path.join(__dirname + "../../views/tickets.ejs")); 
+  }
 };
 module.exports = controller;
--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -4,5 +4,7 @@ const express = require("express");
 const route = express.Router();
 
 route.get("/", controller.index);
+route.get("/fanaticos", controller.fanaticos);
+route.get("/tickets", controller.tickets);
 
 module.exports = route;
